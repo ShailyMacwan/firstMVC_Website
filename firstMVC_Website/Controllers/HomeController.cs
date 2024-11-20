2a8364bc2a34ed38e8c2d7a7d6f7edef1a08b55a
Shaily Macwan <shailymacwan12@gmail.com>
2024-11-20 02:46:50 -0500

    Graph_Commit

--- firstMVC_Website/Controllers/HomeController.cs
+++ firstMVC_Website/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Services.Description;
+using System.Web.UI.WebControls;
 
 namespace firstMVC_Website.Controllers
 {
@@ -130,7 +131,110 @@ namespace firstMVC_Website.Controllers
            
             ViewBag.Name = name;
 
+            /* ----------------------------------------------------------------------------------------------------*/
+
+            List<int> years = new List<int>();
+            List<int> salesData = new List<int>();
+            List<int> revenueData = new List<int>();
+
+            string connectionstring = "Data Source=SHAILY_MACWAN;Initial Catalog=WebsiteData; Integrated Security=True;Connect Timeout=30";
+            string query = "Select Year, sum(Sales) AS totalSales, sum(Revenue) AS totalRevenue FROM SalesData GROUP BY Year ORDER BY Year";
+
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    years.Add(Convert.ToInt32(reader["year"]));
+                    salesData.Add(Convert.ToInt32(reader["totalSales"]));
+                    revenueData.Add(Convert.ToInt32(reader["totalRevenue"]));
+                }
+
+                con.Close();
+            }
+            
+            ViewBag.years= years;
+            ViewBag.salesData = salesData;
+            ViewBag.revenueData = revenueData;
+
+
+
+            /* ----------------------------------------------------------------------------------------------------*/
+
+
+
+
+            /* --------------------------------------------------------------*/
+
+            List<string> months = new List<string>
+{
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+};
+            List<int> salesData1 = new List<int>();
+
+            // Initialize sales data with 0
+            foreach (var _ in months) salesData1.Add(0);
+
+            // SQL Query to fetch monthly sales data
+            string query1 = @"
+    SELECT Month, SUM(Sales) AS TotalSales 
+    FROM SalesData 
+    GROUP BY Month
+    ORDER BY CASE 
+        WHEN Month = 'January' THEN 1
+        WHEN Month = 'February' THEN 2
+        WHEN Month = 'March' THEN 3
+        WHEN Month = 'April' THEN 4
+        WHEN Month = 'May' THEN 5
+        WHEN Month = 'June' THEN 6
+        WHEN Month = 'July' THEN 7
+        WHEN Month = 'August' THEN 8
+        WHEN Month = 'September' THEN 9
+        WHEN Month = 'October' THEN 10
+        WHEN Month = 'November' THEN 11
+        WHEN Month = 'December' THEN 12
+    END;
+";
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    SqlCommand cmd = new SqlCommand(query1, con);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string month = reader["Month"].ToString();
+                        int totalSales = Convert.ToInt32(reader["TotalSales"]);
+
+                        int index = months.IndexOf(month);
+                        if (index >= 0)
+                        {
+                            salesData1[index] = totalSales;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error or handle it
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            ViewBag.Months = months;
+            ViewBag.SalesData1 = salesData1;
+
             return View();
+
+
         }
     }
 }
